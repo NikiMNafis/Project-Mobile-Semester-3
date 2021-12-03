0396f69f2981fa0957ded80f027faa91fec8f70c
Niki Muhammad Nafis <nikinafis08@gmail.com>
2021-12-03 21:06:13 +0700

    update gradle, tambah RegisterActivityTest.java

---
+++ app/src/test/java/com/nikimnafis/testbridgeorganizer/RegisterActivityTest.java
@@ -0,0 +1,36 @@
+package com.nikimnafis.testbridgeorganizer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegisterActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+
+    @Test
+    public void validasiNama() {
+    }
+
+    @Test
+    public void validasiNoTelp() {
+    }
+
+    @Test
+    public void validasiEmail() {
+    }
+
+    @Test
+    public void validasiPassword() {
+    }
+
+    @Test
+    public void validasiUlangPassword() {
+    }
+}
