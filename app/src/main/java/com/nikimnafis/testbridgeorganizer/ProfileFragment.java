9dd7174864fc6a4c23e1e0c092ef855898811d93
Niki Muhammad Nafis <nikinafis08@gmail.com>
2021-12-14 23:44:06 +0700

    update LoginActivity,MainActivity, and others

--- app/src/main/java/com/nikimnafis/testbridgeorganizer/ProfileFragment.java
+++ app/src/main/java/com/nikimnafis/testbridgeorganizer/ProfileFragment.java
@@ -114,7 +114,6 @@ public class ProfileFragment extends Fragment {
 
                     txtNamaUser.setText(namaUser);
                     txtEmailUser.setText(emailUser);
-
                 }
             }
 
